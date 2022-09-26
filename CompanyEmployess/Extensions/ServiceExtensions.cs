d79f8ded4316e0cbd796ae3f2365b2b0878e3e88
egord <egord@EGORICH>
2022-09-26 22:43:31 +0300

    Выполенеие 4 лабораторной.

--- CompanyEmployess/Extensions/ServiceExtensions.cs
+++ CompanyEmployess/Extensions/ServiceExtensions.cs
@@ -23,17 +23,17 @@ namespace CompanyEmployess.Extensions
                 .AllowAnyMethod()
                 .AllowAnyHeader());
             });
-        public static void ConfigureIISIntegration(this IServiceCollection services) =>
-            services.Configure<IISOptions>(options =>
-            {
 
-            });
-        public static void ConfigureLoggerService(this IServiceCollection services) =>
-            services.AddScoped<ILoggerManager, LoggerManager>();
-        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
-            services.AddDbContext<RepositoryContext>(opts => 
-                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("CompanyEmployess")));
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>{});
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>services.AddScoped<ILoggerManager, LoggerManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>services.AddDbContext<RepositoryContext>(opts => 
+        opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("CompanyEmployess")));
+
         public static void ConfigureIRepManagger(this IServiceCollection services) => services.AddScoped<IRepositoryManager, RepositoryManager>();
 
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
     }
 }
