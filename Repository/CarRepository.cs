e79d570157eb0eeb2ac2af8c63e879ea18e05c99
egord <egord@EGORICH>
2022-10-01 16:06:26 +0300

    добавление репозитория.

---
+++ Repository/CarRepository.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Repository
+{
+    public class CarRepository : RepositoryBase<Car>
+    {
+        public CarRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+        {
+        }
+
+
+    
+    }
+}
