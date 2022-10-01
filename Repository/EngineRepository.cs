0aa6bd12f867d400268ddf63ad0182f08906b636
egord <egord@EGORICH>
2022-10-01 16:06:26 +0300

    добавление репозитория.

---
+++ Repository/EngineRepository.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class EngineRepository : RepositoryBase<Engine>
+    {
+        public EngineRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+        {
+        }
+
+
+
+    }
+}
