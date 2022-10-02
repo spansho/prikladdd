ba4d401612955e911f47a61842a6e70c81d83974
egord <egord@EGORICH>
2022-10-02 23:14:51 +0300

    выполняем 7 лабу.

--- Entities/DataTransferObjects/EmployeeForManipulationDto.cs
+++ Entities/DataTransferObjects/EmployeeForManipulationDto.cs
@@ -7,16 +7,15 @@ using System.Threading.Tasks;
 
 namespace Entities.DataTransferObjects
 {
-    public class EmployeeForCreationDto
+    public abstract class EmployeeForManipulationDto
     {
         [Required(ErrorMessage = "Employee name is a required field.")]
         [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30characters.")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Age is a required field.")]
-        [Range(18, int.MaxValue, ErrorMessage = "Age is required and it can't be lower than18")] 
+        [Range(18, int.MaxValue, ErrorMessage = "Age is required and it can't belower than 18")]
         public int Age { get; set; }
         [Required(ErrorMessage = "Position is a required field.")]
-        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20characters.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
         public string Position { get; set; }
     }
 }
