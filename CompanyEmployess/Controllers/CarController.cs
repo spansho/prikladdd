accaab1381eb963275b70166e86bfe2ab817f7af
egord <egord@EGORICH>
2022-10-05 20:52:46 +0300

    3 и 4 лаба вместе.

---
+++ CompanyEmployess/Controllers/CarController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using CompanyEmployess.ModelBinders;
+using Contracts;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompanyEmployess.Controllers
+{
+    [Route("api/engines/{engineId}/cars")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+
+        public CarController(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetCarsForEngine(Guid engineId)
+        {
+            var engine = _repository.Engine.GetEngine(engineId, trackChanges: false);
+            if (engine == null)
+            {
+                _logger.LogInfo($"Company with id: {engineId} doesn't exist in the database.");
+                return NotFound();
+                
+
+            }
+
+            var engines = _repository.Car.GetCars(engineId, trackChanges: false);
+            var enginesDto = _mapper.Map<IEnumerable<CarDto>>(engines);
+            return Ok(enginesDto);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetCarForEngine(Guid engineId, Guid id)
+        {
+            var engine = _repository.Engine.GetEngine(engineId, trackChanges: false);
+            if (engine == null)
+            {
+                _logger.LogInfo($"Company with id: {engineId} doesn't exist in thedatabase.");
+                return NotFound();
+            }
+            var carFromDb = _repository.Car.GetCar(engineId, id, trackChanges: false);
+            if (carFromDb == null)
+            {
+                _logger.LogInfo($"Employee with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            var car = _mapper.Map<CarDto>(carFromDb);
+            return Ok(car);
+        }
+    }
+}
--- Contracts/IEngineRepository.cs
+++ Contracts/IEngineRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace Contracts
 {
     public interface IEngineRepository
     {
-
-
+        IEnumerable<Engine> GetAllEngines(bool trackChanges);
+        Engine GetEngine(Guid EngineyId, bool trackChanges);
     }
 }
